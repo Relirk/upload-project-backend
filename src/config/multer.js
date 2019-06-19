3a529e526b7bcd18cbb0306ecac51a9602498339
Francisco José Olvera Junior <olverajunior2014@gmail.com>
2019-06-19 03:52:38 -0300

    Finalizado deploy aws

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,21 +1,40 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
     filename: (req, file, callback) => {
       crypto.randomBytes(16, (error, hash) => {
         if (error) callback(error);
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+        callback(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.AWS_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, callback) => {
+      crypto.randomBytes(16, (error, hash) => {
+        if (error) callback(error);
         const fileName = `${hash.toString("hex")}-${file.originalname}`;
         callback(null, fileName);
       });
     }
-  }),
+  })
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
 app.use(require("./routes"));
 
 app.listen("3333");
